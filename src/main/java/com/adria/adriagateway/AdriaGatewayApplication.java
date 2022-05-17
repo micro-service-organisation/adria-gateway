efc7564faf96a0c62b9c5845d01272576d10f011
Hamza Ben Taher <hamza.ben.taher@ing.com>
2022-05-17 07:57:41 +0200

    first commit

---
+++ src/main/java/com/adria/adriagateway/AdriaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.adria.adriagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdriaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdriaGatewayApplication.class, args);
+	}
+
+}
